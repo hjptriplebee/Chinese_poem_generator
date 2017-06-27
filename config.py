e2a88033e3ca191342665391b429e5d79c243255
hjptriplebee <huangjipengnju@gmail.com>
2017-06-27 10:35:14 +0800

    config

---
+++ config.py
@@ -0,0 +1,14 @@
+# coding: UTF-8
+'''''''''''''''''''''''''''''''''''''''''''''''''''''
+   file name: config.py
+   create time: 2017年06月25日 星期日 10时56分55秒
+   author: Jipeng Huang
+   e-mail: huangjipengnju@gmail.com
+   github: https://github.com/hjptriplebee
+'''''''''''''''''''''''''''''''''''''''''''''''''''''
+batchSize = 16
+learningRateBase = 0.002
+learningRateDecreaseStep = 80
+epochNum = 100
+
+trainPoems = "poetry.txt" # training file location
